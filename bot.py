6a737e191267b3ee6604f931b32f9bbac25cc4ae
admin199 <andrew14.08.1998@yandex.ru>
2021-01-26 21:00:02 +0300

    Added user_list and necessary getters/setters

--- bot.py
+++ bot.py
@@ -3,6 +3,41 @@ import telebot
 bot = telebot.TeleBot('TOKEN')
 
 white_list = ['coperand']
+user_list = {}
+
+def add_insta_username(message):
+    #TODO: Проверить имя пользователя
+    user_dict = user_list.setdefault(message.from_user.username, {'insta': [], 'vk': []})
+    if message.text not in user_dict['insta']:
+        user_dict['insta'].append(message.text)
+        bot.send_message(message.from_user.id, text="Пользователь добавлен в список отслеживаемых")
+    else:
+        bot.send_message(message.from_user.id, text="Пользователь уже имеется в списке отслеживаемых")
+
+def del_insta_username(message):
+    user_dict = user_list.setdefault(message.from_user.username, {'insta': [], 'vk': []})
+    if message.text not in user_dict['insta']:
+        bot.send_message(message.from_user.id, text="Пользователя нет в списке отслеживаемых")
+    else:
+        user_dict['insta'].remove(message.text)
+        bot.send_message(message.from_user.id, text="Пользователь удален из списка отслеживаемых")
+
+def add_vk_id(message):
+    #TODO: Проверить имя пользователя
+    user_dict = user_list.setdefault(message.from_user.username, {'insta': [], 'vk': []})
+    if message.text not in user_dict['vk']:
+        user_dict['vk'].append(message.text)
+        bot.send_message(message.from_user.id, text="Пользователь добавлен в список отслеживаемых")
+    else:
+        bot.send_message(message.from_user.id, text="Пользователь уже имеется в списке отслеживаемых")
+
+def del_vk_id(message):
+    user_dict = user_list.setdefault(message.from_user.username, {'insta': [], 'vk': []})
+    if message.text not in user_dict['vk']:
+        bot.send_message(message.from_user.id, text="Пользователя нет в списке отслеживаемых")
+    else:
+        user_dict['vk'].remove(message.text)
+        bot.send_message(message.from_user.id, text="Пользователь удален из списка отслеживаемых")
 
 def ask_platform_worker(call):
     if call.data == 'add_user':
@@ -34,6 +69,48 @@ def ask_platform_worker(call):
 def callback_worker(call):
     if call.data == 'add_user' or call.data == 'del_user' or call.data == 'print_users':
         ask_platform_worker(call)
+    
+    if call.data == 'add_user_insta':
+        msg = bot.send_message(call.message.chat.id, text="Введите имя пользователя")
+        bot.register_next_step_handler(msg, add_insta_username)
+    
+    if call.data == 'add_user_vk':
+        msg = bot.send_message(call.message.chat.id, text="Введите id пользователя")
+        bot.register_next_step_handler(msg, add_vk_id)
+    
+    if call.data == 'print_users_insta':
+        print_str = 'Список отслеживаемых пользователей:'
+        user_dict = user_list.setdefault(call.from_user.username, {'insta': [], 'vk': []})
+        for item in user_dict['insta']:
+            print_str += "\n" + item
+        bot.send_message(call.message.chat.id, print_str)
+    
+    if call.data == 'del_user_insta':
+        print_str = 'Введите имя пользователя, которого вы хотите удалить из следующего списка:'
+        user_dict = user_list.setdefault(call.from_user.username, {'insta': [], 'vk': []})
+        for item in user_dict['insta']:
+            print_str += "\n" + item
+        msg = bot.send_message(call.message.chat.id, print_str)
+        bot.register_next_step_handler(msg, del_insta_username)
+    
+    if call.data == 'print_users_vk':
+        print_str = 'Список отслеживаемых пользователей:'
+        user_dict = user_list.setdefault(call.from_user.username, {'insta': [], 'vk': []})
+        for item in user_dict['vk']:
+            print_str += "\n" + item
+        bot.send_message(call.message.chat.id, print_str)
+        #TODO: Вывод имен?
+    
+    if call.data == 'del_user_vk':
+        print_str = 'Введите идентификатор пользователя, которого вы хотите удалить из следующего списка:'
+        user_dict = user_list.setdefault(call.from_user.username, {'insta': [], 'vk': []})
+        for item in user_dict['vk']:
+            print_str += "\n" + item
+        #TODO: Вывод имен?
+        msg = bot.send_message(call.message.chat.id, print_str)
+        bot.register_next_step_handler(msg, del_vk_id)
+    
+    bot.answer_callback_query(call.id)
 
 @bot.message_handler(content_types=['text'])
 def text_messages_handler(message):
